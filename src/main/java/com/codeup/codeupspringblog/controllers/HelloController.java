5383d5ee92ec67c5437090ca5331bb17c4e6ba4e
Lennox Ballard <noellemb@gmail.com>
2023-05-24 11:37:44 -0500

    controller exercise

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/hello/{name1}/and/{name2}")
+    @ResponseBody
+    public String sayHellotwo(@PathVariable String name1, @PathVariable String name2) {
+        return name1 + " says hello to " + name2 + "!";
+    }
+
+//    @GetMapping(path = "/hello", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String sayHelloTwo(@PathVariable String name1, @PathVariable String name2) {
+//        return name1 + " says hello to " + name2 + "!";
+//    }
+}
