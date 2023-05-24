483e1c5ef3b34fb2ba068ff25defb535d49bc1ed
Lennox Ballard <noellemb@gmail.com>
2023-05-24 14:00:57 -0500

    views lesson

--- src/main/java/com/codeup/codeupspringblog/controllers/MathController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/MathController.java
@@ -11,25 +11,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MathController {
     @GetMapping("/add/{number1}/and/{number2}")
     @ResponseBody
-    public int Math(@PathVariable int number1, @PathVariable int number2) {
-        return (number1 + number2);
+    public String Math(@PathVariable int number1, @PathVariable int number2) {
+        return String.valueOf(number1 + number2);
     }
 
     @GetMapping("/subtract/{number1}/from/{number2}")
     @ResponseBody
-    public int Math2(@PathVariable int number1, @PathVariable int number2) {
-        return (number2 - number1);
+    public String Math2(@PathVariable int number1, @PathVariable int number2) {
+        return String.valueOf(number2 - number1);
     }
 
     @GetMapping("/multiply/{number1}/and/{number2}")
     @ResponseBody
-    public int Math3(@PathVariable int number1, @PathVariable int number2) {
-        return (number1 * number2);
+    public String Math3(@PathVariable int number1, @PathVariable int number2) {
+        return String.valueOf(number1 * number2);
     }
 
     @GetMapping("/divide/{number1}/by/{number2}")
     @ResponseBody
-    public int Math4(@PathVariable int number1, @PathVariable int number2) {
-        return (number1 / number2);
+    public String Math4(@PathVariable int number1, @PathVariable int number2) {
+        return String.valueOf(number1 / number2);
     }
 }
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,9 +1,11 @@
 package com.codeup.codeupspringblog.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,15 +25,25 @@ public class PostController {
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String createPost() {
+    public String createForm() {
         return "view the form for creating a post";
     }
 
     @RequestMapping("/posts/create")
     @ResponseBody
-    public String postCreatePost() {
+    public String submitForm() {
         return "create a new post";
     }
+
+//    @RequestMapping(path = "/posts/create", method = {RequestMethod.GET, RequestMethod.POST})
+//    @ResponseBody
+//    public String postsCreate(HttpServletRequest request) {
+//        if (request.getMethod().equals("GET")) {
+//            return "view the form for creating a post";
+//        } else {
+//            return "create a new post";
+//        }
+//    }
 
 }
 
